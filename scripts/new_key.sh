2a68305bbe37d3a99ef413a55679012bcf5c5765
Iker Ruiz de Infante Gonzalez <irzinfante@gmail.com>
2023-01-04 13:28:15 +0100

    Key creation script more verbose

--- scripts/new_key.sh
+++ scripts/new_key.sh
@@ -7,5 +7,13 @@ rm -rf ./keys/$NAME
 mkdir -p ./keys/$NAME
 
 echo $KEY > ./keys/$NAME/key
+
+echo -n "Generating keys for $NAME ."
+
 docker run --rm --entrypoint=/bin/bash hyperledger/besu:22.10.3 -c "echo $KEY > key; besu public-key export-address --node-private-key-file=key --to=address &> /dev/null; cat address" > ./keys/$NAME/address
+
+echo -n "."
+
 docker run --rm --entrypoint=/bin/bash hyperledger/besu:22.10.3 -c "echo $KEY > key; besu public-key export --node-private-key-file=key --to=enode &> /dev/null; cat enode | cut -c 3-" > ./keys/$NAME/enode
+
+echo " done!"
