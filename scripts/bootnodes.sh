01248cefb5573bc9c4288e5cde9d2d83bbe98983
Iker Ruiz de Infante Gonzalez <irzinfante@gmail.com>
2023-01-05 10:51:06 +0100

    Network nodes ports

--- scripts/bootnodes.sh
+++ scripts/bootnodes.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "BESU_BOOTNODES=\
-enode://$(cat ./keys/validator1/enode)@100.20.5.1:30303,\
-enode://$(cat ./keys/validator2/enode)@100.20.5.2:30303,\
-enode://$(cat ./keys/validator3/enode)@100.20.5.3:30303,\
-enode://$(cat ./keys/validator4/enode)@100.20.5.4:30303" > bootnodes.env
+enode://$(cat ./keys/validator1/enode)@127.0.0.1:30301,\
+enode://$(cat ./keys/validator2/enode)@127.0.0.1:30302,\
+enode://$(cat ./keys/validator3/enode)@127.0.0.1:30303,\
+enode://$(cat ./keys/validator4/enode)@127.0.0.1:30304" > bootnodes.env
